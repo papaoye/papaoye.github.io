e65f3063ba6e67d34a340a3b9e5a415302c89e16
Prakash <prakasid>
2016-01-24 20:12:19 +0530

    new template

---
+++ components/home.js
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+
+angular.module('dreamApp', ['ngSanitize']).controller('HomeController', function($http, $q, $log, $scope) {
+	
+	$log.info("Starting home module...");
+	
+	var homeModel = this;
+
+	homeModel.servicesSection = "";
+	homeModel.portfolioSecion = "";
+	
+	$http.get('/fragments/services.html')
+    .success(function (data) {
+    	homeModel.servicesSection = data;
+    }).error(function (msg, code) {
+        $log.error(msg, code);
+    });
+	
+//	$http.get('/fragments/portfolio.html')
+//    .success(function (data) {
+//    	homeModel.portfolioSecion = data;
+//    }).error(function (msg, code) {
+//        $log.error(msg, code);
+//    });
+	
+	
+});
